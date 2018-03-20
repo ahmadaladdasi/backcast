01dc1ed3af10c7d9ec3b519eb6484afbfb08f206
Evelyn <evelynchan92@gmail.com>
2018-03-20 10:06:15 -0700

    updated app view

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,16 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(exampleVideoData);
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    new VideoListView({el: '.list', collection: this.videos}).render();
+
+    new VideoPlayerView({el: '.player', collection: this.videos}).render();
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -4,6 +4,9 @@ var VideoListView = Backbone.View.extend({
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    this.$el.children().append(this.collection.map(video => new VideoListEntryView({model: video}).render().el))
+
     return this;
   },
 
